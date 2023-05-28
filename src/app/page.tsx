6d9fe43d2cd09b1e09be19a50243da47ae419372
Claudio Fuentes <imclaudfuen@gmail.com>
2023-05-28 16:28:52 -0700

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import HomePage from "@/components/HomePage";
 import { supabase } from "@/lib/supabase";
+import { Metadata } from "next";
 
 export const revalidate = 1;
+
+export const metadata = {
+  title: "Debate Tube | Watch AIs Debate each-other",
+  description: "Watch AIs debating each other on controversial topics.",
+} as Metadata;
 
 export default async function Home() {
   const { data: messages, error } = await supabase
